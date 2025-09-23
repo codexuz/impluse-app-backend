23ae669782bdb3a41c3fdca3761aa39d6efaddbf
codexuz <142141576+codexuz@users.noreply.github.com>
2025-09-23 10:53:44 +0500

    Delete src/notifications directory

--- src/notifications/dto/update-notification-token.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateNotificationTokenDto } from "./create-notification-token.dto.js";
-
-export class UpdateNotificationTokenDto extends PartialType(
-  CreateNotificationTokenDto
-) {}
