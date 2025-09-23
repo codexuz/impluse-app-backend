7bf8b2b2a81784c5dec9a1f6841a33b4fdaa6b71
codexuz <142141576+codexuz@users.noreply.github.com>
2025-09-23 11:02:06 +0500

    Delete src/attendance directory

--- src/attendance/dto/update-attendance.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAttendanceDto } from './create-attendance.dto.js';
-
-export class UpdateAttendanceDto extends PartialType(CreateAttendanceDto) {}
