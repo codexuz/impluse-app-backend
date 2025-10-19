f6713b785ffc74168eff01879230f082665df6f3
codexuz <142141576+codexuz@users.noreply.github.com>
2025-10-19 16:02:05 +0500

    Update ieltspart3-question.controller.ts

--- src/ieltspart3-question/ieltspart3-question.controller.ts
+++ src/ieltspart3-question/ieltspart3-question.controller.ts
@@ -69,7 +69,7 @@ export class Ieltspart3QuestionController {
     }
 
     @Delete(':id')
-    @Roles(Role.ADMIN)
+    @Roles(Role.ADMIN, Role.TEACHER)
     @ApiOperation({ summary: 'Delete an IELTS Part 3 question' })
     @ApiResponse({ status: 200, description: 'The question has been successfully deleted.' })
     @ApiResponse({ status: 401, description: 'Unauthorized.' })
@@ -79,3 +79,4 @@ export class Ieltspart3QuestionController {
         return await this.ieltspart3QuestionService.remove(id);
     }
 }
+
