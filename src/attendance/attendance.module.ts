ec6ec452a94849a0b532ee0606a3ed567b689575
codexuz <142141576+codexuz@users.noreply.github.com>
2025-10-30 11:46:48 +0500

    Enhance AttendanceModule with Sequelize support
    Added Sequelize integration and exports for AttendanceService.

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AttendanceService } from './attendance.service.js';
-import { AttendanceController } from './attendance.controller.js';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { AttendanceService } from "./attendance.service.js";
+import { AttendanceController } from "./attendance.controller.js";
+import { Attendance } from "./entities/attendance.entity.js";
+import { TeacherProfile } from "../teacher-profile/entities/teacher-profile.entity.js";
+import { TeacherWallet } from "../teacher-wallet/entities/teacher-wallet.entity.js";
+import { TeacherTransaction } from "../teacher-transaction/entities/teacher-transaction.entity.js";
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([
+      Attendance,
+      TeacherProfile,
+      TeacherWallet,
+      TeacherTransaction,
+    ]),
+  ],
   controllers: [AttendanceController],
   providers: [AttendanceService],
+  exports: [AttendanceService],
 })
 export class AttendanceModule {}
