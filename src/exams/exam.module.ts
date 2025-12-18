baa9592e417001eb67ada59ab1b140e67d4168af
codexuz <examonlineedumo@gmail.com>
2025-12-18 16:06:08 +0500

    "main"

--- src/exams/exam.module.ts
+++ src/exams/exam.module.ts
@@ -4,6 +4,7 @@ import { Exam } from "./entities/exam.entity.js";
 import { ExamResult } from "./entities/exam_result.entity.js";
 import { GroupStudent } from "../group-students/entities/group-student.entity.js";
 import { Group } from "../groups/entities/group.entity.js";
+import { User } from "../users/entities/user.entity.js";
 import { ExamService } from "./exam.service.js";
 import { ExamController } from "./exam.controller.js";
 import { ExamResultsService } from "./exam-results.service.js";
@@ -11,7 +12,7 @@ import { ExamResultsController } from "./exam-results.controller.js";
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Exam, GroupStudent, Group, ExamResult]),
+    SequelizeModule.forFeature([Exam, GroupStudent, Group, ExamResult, User]),
   ],
   controllers: [ExamController, ExamResultsController],
   providers: [ExamService, ExamResultsService],
