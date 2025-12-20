6905a19173dd1cf9aad52869da96c593356b89fd
codexuz <examonlineedumo@gmail.com>
2025-12-20 13:00:29 +0500

    "main"

--- src/vocabulary_sets/vocabulary_sets.module.ts
+++ src/vocabulary_sets/vocabulary_sets.module.ts
@@ -3,10 +3,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { VocabularySetsService } from './vocabulary_sets.service.js';
 import { VocabularySetsController } from './vocabulary_sets.controller.js';
 import { VocabularySet } from './entities/vocabulary_set.entity.js';
+import { VocabularyItem } from '../vocabulary_items/entities/vocabulary_item.entity.js';
+import { StudentVocabularyProgress } from '../student_vocabulary_progress/entities/student_vocabulary_progress.entity.js';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([VocabularySet])
+    SequelizeModule.forFeature([VocabularySet, VocabularyItem, StudentVocabularyProgress])
   ],
   controllers: [VocabularySetsController],
   providers: [VocabularySetsService],
