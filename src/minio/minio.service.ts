3ce2781600a10841b61a169966865eda5568e01e
codexuz <examonlineedumo@gmail.com>
2025-12-30 19:12:13 +0500

    "main"

--- src/minio/minio.service.ts
+++ src/minio/minio.service.ts
@@ -8,11 +8,11 @@ export class MinioService {
 
   constructor() {
     this.minioClient = new Minio.Client({
-      endPoint: "play.min.io",
-      port: 9000,
-      useSSL: true,
-      accessKey: "Q3AM3UQ867SPQQA43P2F",
-      secretKey: "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+      endPoint: process.env.MINIO_ENDPOINT,
+      port: parseInt(process.env.MINIO_PORT),
+      useSSL: process.env.MINIO_USE_SSL === "true",
+      accessKey: process.env.MINIO_ACCESS_KEY,
+      secretKey: process.env.MINIO_SECRET_KEY,
     });
   }
 
