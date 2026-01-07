e706605d1853c69afca6f074833a589b310597f7
codexuz <examonlineedumo@gmail.com>
2026-01-07 23:43:52 +0500

    "main"

--- src/student-parents/dto/update-student-parent.dto.ts
+++ src/student-parents/dto/update-student-parent.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateStudentParentDto } from './create-student-parent.dto';
+import { CreateStudentParentDto } from './create-student-parent.dto.js';
 
 export class UpdateStudentParentDto extends PartialType(CreateStudentParentDto) {}
